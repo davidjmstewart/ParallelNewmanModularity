3ef31cf29a9b73b1f24ea3934737a942e011de91
unknown <davidjmstewart@gmail.com>
2020-10-03 17:45:25 +1000

    Added some basic error checking to ensure we successfully allocate enough memory for the adjacency matrix. Added a call to free the allocate the memory

--- main.c
+++ main.c
@@ -3,10 +3,17 @@
 
 #include "./lib/CDUTils.h"
 
-unsigned long const MATRIX_SIZE = 500;
+unsigned long const MATRIX_SIZE = 5000;
+
 int main(int argc, char *argv[]) {
     int *A = (int *)malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(int));
-
+    if (A == NULL) {
+        printf("Could not allocate %lu bytes to A. Exiting program\n", MATRIX_SIZE * MATRIX_SIZE * sizeof(int));
+        return -1;
+    }
     printf("Generating %lu x %lu adjacency matrix \n", MATRIX_SIZE, MATRIX_SIZE);
     genAdjacenyMatrix(A, MATRIX_SIZE);
+
+    free(A);
+    return 0;
 }
